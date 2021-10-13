1f48f9a2c264e718f9ed90e4511d3f35c09d7a55
Robin Kelsick II <robinkelsickii@Armandos-MBP-2.localdomain>
2021-10-13 15:35:43 -0400

    installed react, bootstrap, and popperjs. Initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
